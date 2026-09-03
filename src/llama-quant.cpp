42fc27495ab92308d112af71900fa5896516c8ff
crush b <crush@b3.lt>
2026-09-03 20:07:45 +1200

    interim checkin to quantise MXFP8

--- src/llama-quant.cpp
+++ src/llama-quant.cpp
@@ -858,6 +858,7 @@ ggml_type llama_ftype_get_default_type(llama_ftype ftype) {
         case LLAMA_FTYPE_MOSTLY_Q2_0: return GGML_TYPE_Q2_0;
 
         case LLAMA_FTYPE_MOSTLY_MXFP4_MOE: return GGML_TYPE_MXFP4;
+        case LLAMA_FTYPE_MOSTLY_MXFP8:     return GGML_TYPE_MXFP8;
 
         // K-quants
         case LLAMA_FTYPE_MOSTLY_Q2_K_S:
